74aab95fda57b18dadbf4ec362b829a903a43760
Sway Wang <sway.khwang@gmail.com>
2018-03-30 15:44:13 +0800

    remove using namespace std

--- poj/1000.cpp
+++ poj/1000.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
     int a,b;
-    cin >> a >> b;
-    cout << a+b << endl;
-        return 0;
+    std::cin >> a >> b;
+    std::cout << a+b << std::endl;
+    return 0;
 }
